acb50802b0b1eb83b80d70b138d4805332e22d40
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-04-21 22:42:37 +0200

    tests for Aged Brie

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -10,7 +10,7 @@ class GildedRoseTest(unittest.TestCase):
         return items[0]
         
     def test_foo_before_sellin(self):
-        item = self.update_item(name="foo", sell_in=11, quality=50)
+        item = self.update_item(name="foo", sell_in=10, quality=50)
         self.assertEquals(49, item.quality)
 
     def test_foo_after_sellin(self):
@@ -21,37 +21,13 @@ class GildedRoseTest(unittest.TestCase):
         item = self.update_item(name="foo", sell_in=0, quality=0)
         self.assertEquals(0, item.quality)
 
-    def test_Brie(self):
-        name = "Aged Brie"
-        sell_in = [11, 10, 6, 5, 0, -1]
-        quality = [50, 49, 1, 0]
-        items = [Item(name, s, q) for s in sell_in for q in quality]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals(50, items[0].quality)
-        self.assertEquals(50, items[1].quality)
-        self.assertEquals(2, items[2].quality)
-        self.assertEquals(1, items[3].quality)
-        self.assertEquals(50, items[4].quality)
-        self.assertEquals(50, items[5].quality)
-        self.assertEquals(2, items[6].quality)
-        self.assertEquals(1, items[7].quality)
-        self.assertEquals(50, items[8].quality)
-        self.assertEquals(50, items[9].quality)
-        self.assertEquals(2, items[10].quality)
-        self.assertEquals(1, items[11].quality)
-        self.assertEquals(50, items[12].quality)
-        self.assertEquals(50, items[13].quality)
-        self.assertEquals(2, items[14].quality)
-        self.assertEquals(1, items[15].quality)
-        self.assertEquals(50, items[16].quality)
-        self.assertEquals(50, items[17].quality)
-        self.assertEquals(3, items[18].quality)
-        self.assertEquals(2, items[19].quality)
-        self.assertEquals(50, items[20].quality)
-        self.assertEquals(50, items[21].quality)
-        self.assertEquals(3, items[22].quality)
-        self.assertEquals(2, items[23].quality)
+    def test_brie_increases(self):
+        item = self.update_item(name = "Aged Brie", sell_in=10, quality=5)
+        self.assertEquals(6, item.quality)
+
+    def test_brie_no_more_than_fifty(self):
+        item = self.update_item(name="Aged Brie", sell_in=10, quality=50)
+        self.assertEquals(50, item.quality)
 
     def test_Sulfuras(self):
         name = "Sulfuras"
