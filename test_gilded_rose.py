a620ff787484ceca2a0905a3cfbffb0e8381735a
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-04-22 20:05:56 +0200

    tests for Conjured item

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -57,37 +57,17 @@ class GildedRoseTest(unittest.TestCase):
         item = self.update_item(name="Backstage", sell_in=0, quality=50)
         self.assertEquals(0, item.quality)
 
-    def test_Conjured(self):
-        name = "Conjured"
-        sell_in = [11, 10, 6, 5, 0, -1]
-        quality = [50, 49, 1, 0]
-        items = [Item(name, s, q) for s in sell_in for q in quality]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals(48, items[0].quality)
-        self.assertEquals(47, items[1].quality)
-        self.assertEquals(0, items[2].quality)
-        self.assertEquals(0, items[3].quality)
-        self.assertEquals(48, items[4].quality)
-        self.assertEquals(47, items[5].quality)
-        self.assertEquals(0, items[6].quality)
-        self.assertEquals(0, items[7].quality)
-        self.assertEquals(48, items[8].quality)
-        self.assertEquals(47, items[9].quality)
-        self.assertEquals(0, items[10].quality)
-        self.assertEquals(0, items[11].quality)
-        self.assertEquals(48, items[12].quality)
-        self.assertEquals(47, items[13].quality)
-        self.assertEquals(0, items[14].quality)
-        self.assertEquals(0, items[15].quality)
-        self.assertEquals(46, items[16].quality)
-        self.assertEquals(45, items[17].quality)
-        self.assertEquals(0, items[18].quality)
-        self.assertEquals(0, items[19].quality)
-        self.assertEquals(46, items[20].quality)
-        self.assertEquals(45, items[21].quality)
-        self.assertEquals(0, items[22].quality)
-        self.assertEquals(0, items[23].quality)
+    def test_conjured_before_sellin(self):
+        item = self.update_item(name="Conjured", sell_in=10, quality=50)
+        self.assertEquals(48, item.quality)
+
+    def test_conjured_after_sellin(self):
+        item = self.update_item(name="Conjured", sell_in=0, quality=50)
+        self.assertEquals(46, item.quality)
+
+    def test_conjured_zero_quality_after_sellin(self):
+        item = self.update_item(name="Conjured", sell_in=0, quality=1)
+        self.assertEquals(0, item.quality)
 
 if __name__ == '__main__':
     unittest.main()
