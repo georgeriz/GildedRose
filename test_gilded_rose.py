c54dccf5773747ae9e57cbf26cb3d1770da21ee1
Georgios Rizothanasis <trivialgeorge@gmail.com>
2018-04-22 20:00:02 +0200

    tests for Backstage item

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -29,7 +29,6 @@ class GildedRoseTest(unittest.TestCase):
         item = self.update_item(name="Aged Brie", sell_in=10, quality=50)
         self.assertEquals(50, item.quality)
 
-
     def test_sulfuras_before_sellin(self):
         item = self.update_item(name="Sulfuras", sell_in=10, quality=80)
         self.assertEquals(80, item.quality)
@@ -38,37 +37,25 @@ class GildedRoseTest(unittest.TestCase):
         item = self.update_item(name="Sulfuras", sell_in=0, quality=80)
         self.assertEquals(80, item.quality)  
 
-    def test_Backstage(self):
-        name = "Backstage"
-        sell_in = [11, 10, 6, 5, 0, -1]
-        quality = [50, 49, 1, 0]
-        items = [Item(name, s, q) for s in sell_in for q in quality]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals(50, items[0].quality)
-        self.assertEquals(50, items[1].quality)
-        self.assertEquals(2, items[2].quality)
-        self.assertEquals(1, items[3].quality)
-        self.assertEquals(50, items[4].quality)
-        self.assertEquals(50, items[5].quality)
-        self.assertEquals(3, items[6].quality)
-        self.assertEquals(2, items[7].quality)
-        self.assertEquals(50, items[8].quality)
-        self.assertEquals(50, items[9].quality)
-        self.assertEquals(3, items[10].quality)
-        self.assertEquals(2, items[11].quality)
-        self.assertEquals(50, items[12].quality)
-        self.assertEquals(50, items[13].quality)
-        self.assertEquals(4, items[14].quality)
-        self.assertEquals(3, items[15].quality)
-        self.assertEquals(0, items[16].quality)
-        self.assertEquals(0, items[17].quality)
-        self.assertEquals(0, items[18].quality)
-        self.assertEquals(0, items[19].quality)
-        self.assertEquals(0, items[20].quality)
-        self.assertEquals(0, items[21].quality)
-        self.assertEquals(0, items[22].quality)
-        self.assertEquals(0, items[23].quality)
+    def test_backstage_after_ten_sellin(self):
+        item = self.update_item(name="Backstage", sell_in=10, quality=5)
+        self.assertEquals(7, item.quality)
+
+    def test_backstage_no_more_than_fifty_after_ten_sellin(self):
+        item = self.update_item(name="Backstage", sell_in=10, quality=50)
+        self.assertEquals(50, item.quality)
+
+    def test_backstage_after_five_sellin(self):
+        item = self.update_item(name="Backstage", sell_in=5, quality=5)
+        self.assertEquals(8, item.quality)
+
+    def test_backstage_no_more_than_fifty_after_five_sellin(self):
+        item = self.update_item(name="Backstage", sell_in=5, quality=50)
+        self.assertEquals(50, item.quality)
+
+    def test_backstage_after_sellin(self):
+        item = self.update_item(name="Backstage", sell_in=0, quality=50)
+        self.assertEquals(0, item.quality)
 
     def test_Conjured(self):
         name = "Conjured"
